f0a479ea682845e1eb1a7c1d4405fdf24f1bb6f7
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-06-07 10:34:22 +0530

    Initial commit

---
+++ frontend/src/Context/StoreContext.jsx
@@ -0,0 +1,18 @@
+import React , {createContext} from 'react';
+export const StoreContext = createContext(null);
+import { food_list } from '../assets/assets'
+
+const StoreContextProvider = (props) => {
+
+    const contextValue = {
+            food_list
+    }
+    return (
+        <StoreContext.Provider value  = {contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+
+}
+
+export default StoreContextProvider 
