21fcf7749cb7aa6c1a34ad1559c4b1879cee1beb
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-07-02 21:56:30 +0530

    Work on order page backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,6 +6,9 @@ import Stripe from "stripe";
 
 
 
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY is not defined in environment variables");
+}
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
@@ -18,8 +21,9 @@ const placeOrder = async (req, res) => {
             amount : req.body.amount,
             address: req.body.address
         });
+
         await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: [] });
         
         const line_items =req.body.items.map((item)=>({
                 price_data: {
@@ -56,4 +60,54 @@ const placeOrder = async (req, res) => {
     }
 };
 
-export {placeOrder} 
+const verifyOrder =async (req,res)=>{
+    const {orderId,success}=req.body;
+    try {
+        if(success=="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message :"Paid"})
+        }else{
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({success:false,message:"NOt Paid"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+
+}
+
+//  user order for frontend 
+const userOrders = async(req,res)=>{
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+// listing orders for admin panel
+const  listOrders = async (req,res)=>{
+    try {
+        const orders = await orderModel.find({}); 
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+} 
+
+// api for updating order status 
+const updateStatus =async (req,res)=>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"});
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export  {placeOrder,verifyOrder,userOrders,listOrders,updateStatus} ;
