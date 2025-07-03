6075355e3b870eb59b1fe15ca44a86c120509d87
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-07-03 15:04:48 +0530

    fixing multer for vercel

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -3,16 +3,11 @@ import { addFood , listFood ,removeFood } from "../controllers/foodController.js
 import multer from "multer"
 
 const foodRouter = express.Router();
-    const storage = multer.diskStorage({
 
-        destination: "uploads",
-        filename:(req, file, cb)=>{
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-           return cb(null, `${Date.now()}${file.originalname}`)
-        }
-})
-const upload = multer({storage:storage})
-foodRouter.post("/add",upload.single("image"), addFood)
+foodRouter.post("/add", upload.single("image"), addFood)
 foodRouter.get("/list", listFood)
 foodRouter.post("/remove", removeFood);
 
