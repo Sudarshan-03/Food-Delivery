d9aadbbb5f46b9424989f7bb9e857ccda540fc51
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-07-03 22:20:14 +0530

    Dark theme Fix , ready for Deploy

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 4000 ;
 
 app.use(express.json());
 app.use(cors());
