5219abf01e1a2296f259466df1ba570bb426b20e
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-09-01 15:25:16 +0530

    Fixxing some Admin Issues

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,13 +1,13 @@
 import express from "express"
 import auth from "../middleware/auth.js"
-import{ placeOrder , verifyOrder, userOrders } from "../controllers/orderController.js"
+import{ placeOrder , verifyOrder, userOrders ,listOrders, updateStatus  } from "../controllers/orderController.js"
 
 const orderRouter = express.Router ();
 
 orderRouter.post("/place" , auth , placeOrder);
 orderRouter.post("/verify" , verifyOrder);
 orderRouter.post("/userorders" , auth , userOrders);
-//orderRouter.get('/add', listOrders);
-//orderRouter.post('/status', updateStatus);
+orderRouter.get('/add', listOrders);
+orderRouter.post('/status', updateStatus);
 
 export default orderRouter;
