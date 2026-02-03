b24ad0e4e66b7333c9483a33c4177c4accd3a375
Sudarshan <sudarshan@Sudarshans-Air.local>
2026-02-03 13:49:34 +0530

    admin nav

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import { assets } from '../../assets/assets';
 const Navbar = ({ handleLogout }) => {
     return (
         <div className='navbar'>
-           <a href="https://zestybites.onrender.com/" target="_blank" rel="noopener noreferrer">
-  <img src={assets.logo2} className="logo" alt="ZestyBites" />
-</a>
+            <a href="https://food-delivery-snowy-nu.vercel.app/" target="_blank" rel="noopener noreferrer">
+                <img src={assets.logo2} className="logo" alt="ZestyBites" />
+            </a>
             <div className="navbar-right">
                 <img src={assets.profile_image} className='profile' alt="" />
                 <button onClick={handleLogout} className="logout-button">Logout</button>
